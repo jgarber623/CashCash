e6e23683eed19046855941b4a13ed2c8fca5cd28
Jason Garber <jason@sixtwothree.org>
2016-12-27 10:54:46 -0500

    Rewrite build and stats scripts to use ES6 syntax goodness.

--- scripts/build.js
+++ scripts/build.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
 
-var colors = require('colors'),
-	exec = require('child_process').exec,
-	pkg = require('../package.json'),
-	preamble = '/*!\n' +
-		' *  CashCash ' + pkg.version + '\n' +
-		' *\n' +
-		' *  ' + pkg.description + '\n' +
-		' *\n' +
-		' *  Source code available at: ' + pkg.homepage + '\n' +
-		' *\n' +
-		' *  (c) 2016-present ' + pkg.author.name + ' (' + pkg.author.url + ')\n' +
-		' *\n' +
-		' *  CashCash may be freely distributed under the ' + pkg.license + ' license.\n' +
-		' */\n';
+let colors = require('colors');
+let childProcess = require('child_process');
+let pkg = require('../package.json');
 
-exec('$(npm bin)/uglifyjs src/cashcash.js --beautify "indent-level=2" --preamble "' + preamble + '" --output dist/cashcash.js');
-exec('$(npm bin)/uglifyjs src/cashcash.js --compress --mangle --preamble "' + preamble + '" --output dist/cashcash.min.js');
+let preamble = `/*!
+ *  CashCash ${pkg.version}
+ *
+ *  ${pkg.description}
+ *
+ *  Source code available at: ${pkg.homepage}
+ *
+ *  (c) 2016-present ${pkg.author.name} (${pkg.author.url})
+ *
+ *  CashCash may be freely distributed under the ${pkg.license} license.
+ */
+`;
 
-console.log(colors.green('CashCash %s built successfully!'), pkg.version);
+childProcess.exec(`$(npm bin)/uglifyjs src/cashcash.js --beautify 'indent-level=2' --preamble '${preamble}' --output dist/cashcash.js`);
+childProcess.exec(`$(npm bin)/uglifyjs src/cashcash.js --compress --mangle --preamble '${preamble}' --output dist/cashcash.min.js`);
+
+console.log(colors.green(`CashCash ${pkg.version} built successfully!`));
--- scripts/stats.js
+++ scripts/stats.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 
-var cliTable = require('cli-table'),
-	commaNumber = require('comma-number'),
-	fs = require('fs'),
-	gzipSize = require('gzip-size'),
-	table = new cliTable({
-		style: {
-			head: ['cyan']
-		}
-	});
+let cliTable = require('cli-table');
+let commaNumber = require('comma-number');
+let fs = require('fs');
+let gzipSize = require('gzip-size');
+
+let table = new cliTable({
+	style: {
+		head: ['cyan']
+	}
+});
 
 table.push(
-	{ 'Uncompressed': commaNumber(fs.statSync('dist/cashcash.js').size) + ' bytes' },
-	{ 'Minified': commaNumber(fs.statSync('dist/cashcash.min.js').size) + ' bytes' },
-	{ 'Minified and gzipped': commaNumber(gzipSize.sync(fs.readFileSync('dist/cashcash.min.js'))) + ' bytes' }
+	{ 'Uncompressed': `${commaNumber(fs.statSync('dist/cashcash.js').size)} bytes` },
+	{ 'Minified': `${commaNumber(fs.statSync('dist/cashcash.min.js').size)} bytes` },
+	{ 'Minified + gzipped': `${commaNumber(gzipSize.sync(fs.readFileSync('dist/cashcash.min.js')))} bytes` }
 );
 
 console.log(table.toString());
