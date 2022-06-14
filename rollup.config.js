43b0e800c8a50604aa6cdc04ccfa0549abf4f99f
Jason Garber <jason@sixtwothree.org>
2022-06-13 23:17:26 -0400

    Rebuild manifest and Rollup config files

--- rollup.config.js
+++ rollup.config.js
@@ -1,77 +1,44 @@
 import pkg from './package.json';
 
-import filesize from 'rollup-plugin-filesize';
 import { terser } from 'rollup-plugin-terser';
 
-const packageName = 'CashCash';
-const releaseYear = 2016;
-const srcFilePath = 'src/cashcash.js';
-
-const filesizePluginOptions = {
-  format: {
-    exponent: 0,
-    fullform: true
-  },
-  theme: 'light'
-};
-
 const preamble = `/*!
- *  ${packageName} v${pkg.version}
+ *  ${pkg.config.displayName} v${pkg.version}
  *
  *  ${pkg.description}
  *
  *  Source code available at: ${pkg.homepage}
  *
- *  (c) ${releaseYear}-present ${pkg.author.name} (${pkg.author.url})
+ *  (c) ${pkg.config.licenseYear}-present ${pkg.author.name} (${pkg.author.url})
  *
- *  ${packageName} may be freely distributed under the ${pkg.license} license.
+ *  ${pkg.config.displayName} may be freely distributed under the ${pkg.license} license.
  */
 `;
 
-export default [
-  // cashcash.mjs and cashcash.js
-  {
-    input: srcFilePath,
-    output: [
-      {
-        file: pkg.module,
-        format: 'es'
-      },
-      {
-        file: pkg.main,
-        format: 'umd',
-        name: packageName
-      }
-    ],
-    plugins: [
-      filesize(filesizePluginOptions),
-      terser({
-        compress: false,
-        mangle: false,
-        output: {
-          beautify: true,
-          indent_level: 2,
-          preamble: preamble
-        }
-      })
-    ]
-  },
+const terserConfig = {
+  compress: false,
+  mangle: false,
+  output: {
+    beautify: true,
+    indent_level: 2,
+    preamble: preamble
+  }
+};
 
-  // cashcash.min.js
+export default ({ input }) => [
   {
-    input: srcFilePath,
-    output: {
-      file: pkg.browser,
-      format: 'umd',
-      name: packageName
-    },
-    plugins: [
-      filesize(filesizePluginOptions),
-      terser({
-        output: {
-          preamble: preamble
-        }
-      })
-    ]
+    input: input,
+    output: { file: pkg.module, format: 'es' },
+    plugins: [terser(terserConfig)]
+  },
+  {
+    input: input,
+    output: { file: pkg.main, format: 'umd', name: pkg.config.displayName },
+    plugins: [terser(terserConfig)]
+  },
+  {
+    input: input,
+    output: { file: pkg.browser, format: 'umd', name: pkg.config.displayName },
+    plugins: [terser({ output: { preamble: preamble }})]
   }
 ];
