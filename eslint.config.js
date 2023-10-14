061d4d0c6f0f81d37133ffd45a76e553840009db
Jason Garber <jason@sixtwothree.org>
2023-10-14 09:02:17 -0400

    Streamline ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,8 @@
 import config from '@jgarber/eslint-config';
-import globals from 'globals';
 
 export default [
   {
     ignores: ['dist']
   },
-  ...config,
-  {
-    files: ['**/*.?(m)js'],
-    languageOptions: {
-      globals: {
-        ...globals.browser
-      }
-    }
-  },
-  {
-    files: ['spec/**/*[sS]pec.?(m)js'],
-    languageOptions: {
-      globals: {
-        ...globals.jasmine
-      }
-    }
-  }
+  ...config
 ];
