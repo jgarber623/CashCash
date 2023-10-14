dce6c89ba7917baef2361aeff7e169067dd970fa
Jason Garber <jason@sixtwothree.org>
2023-10-14 09:07:45 -0400

    Breaking: Convert project to ESM (#126)
    * Rename and refactor Rollup config file
    
    * Convert ESLint config to ESM
    
    * Breaking: Set minimum supported Node.js version
    
    Also breaking: change how exports work, set type to module.
    
    Really just break all the things.
    
    * Update packages, remove globals package
    
    * Streamline ESLint config
    
    * Rename source file

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,8 @@
-const config = require('@jgarber/eslint-config');
-const globals = require('globals');
+import config from '@jgarber/eslint-config';
 
-module.exports = [
-  ...config,
+export default [
   {
-    ignores: ['dist/*.?(m)js']
+    ignores: ['dist']
   },
-  {
-    files: ['**/*.?(m)js'],
-    languageOptions: {
-      globals: {
-        ...globals.browser
-      }
-    }
-  },
-  {
-    files: ['spec/**/*[sS]pec.?(m)js'],
-    languageOptions: {
-      globals: {
-        ...globals.jasmine
-      }
-    }
-  }
+  ...config
 ];
